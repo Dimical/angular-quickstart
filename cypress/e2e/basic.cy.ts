1e3bbf303a745e90a17d05ec169a4c7474b75298
Dimic <dimic@example.com>
2025-02-16 21:01:03 +0100

    modif

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -4,14 +4,15 @@ describe('empty spec', () => {
   })
 
   it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Angular project that has everything you need to quickly deploy it to Netlify');
+    cy.get('h2') // <-- Vérifie que le sélecteur est correct avec Selector Playground
+      .contains('This is a bare-bones Angular project that has everything you need to quickly deploy it to Netlify'); // <-- Vérifie que le texte est EXACTEMENT le même (copie-colle)
   })
+
   it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
+    cy.get('img') // <-- Vérifie que le sélecteur est correct avec Selector Playground
+      .should('be.visible')
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      })
   })
 })
